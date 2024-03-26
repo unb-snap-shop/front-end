0234e8a891a1274b3a2ebfc685c7eb7e03b6d24f
Eric599 <cuenat.eric@gmail.com>
2024-03-26 17:32:02 -0300

    add cart endpoint test

--- src/views/Cart.jsx
+++ src/views/Cart.jsx
@@ -1,13 +1,42 @@
-import { React } from "react"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Product from "../components/product/Product";
 
 
 const Cart = () => {
 
-    return (
-        <h1>
-            This is a cart!
-        </h1>
-    )
+
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Function to fetch cart from Flask API
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get("http://localhost:5001/test")
+        setProducts(response.data)
+        console.log(response.data)
+      } catch (error) {
+        const errorMessage = error.response ? `HTTP error! Status: ${error.response.status}` : error.message;
+        setError(errorMessage);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Test Endpoint</h1>
+      <div>
+        <h1>{products}</h1> 
+      </div>
+    </div>
+  );
 
 }
 
